d57d1bf32c5f9f71606a6b821eada6e7374c4b36
Wang YC <956882148@qq.com>
2023-07-27 17:03:53 +0800

    Added blockchain parameters

--- apis/jsonrpc_api.py
+++ apis/jsonrpc_api.py
@@ -1,7 +1,9 @@
 """完成测试数据的组装, 通过调用不同的接口来实现具体业务逻辑"""
+
 import os
 from common.handle_path import CONF_DIR
 from common.utils import Utils
+from common.wrapper import api_call
 
 
 class JsonrpcApi:
@@ -11,6 +13,7 @@ class JsonrpcApi:
     conf_data = Utils.handle_yaml(conf_path)  # 调用工具类封装的读取文件的方法
     headers = conf_data['request_headers']['headers']
 
+    @api_call
     def eth_chainId(self, url, **data):
         """返回当前配置的链 ID"""
         payload = {
@@ -22,6 +25,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_syncing(self, url, **data):
         """返回一个对象，其中包含有关同步状态的数据或 false"""
         payload = {
@@ -33,6 +37,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_getBlockByNumber(self, url, **data):
         """根据区块编号返回关于区块的信息"""
         payload = {
@@ -44,6 +49,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_getBlockByHash(self, url, **data):
         """根据哈希返回区块信息"""
         payload = {
@@ -55,6 +61,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_blockNumber(self, url, **data):
         """返回最新区块的编号"""
         payload = {
@@ -66,6 +73,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_gasPrice(self, url, **data):
         """返回以 wei 为单位的当前 gas 价格"""
         payload = {
@@ -77,6 +85,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_getBalance(self, url, **data):
         """返回给定地址的账户余额"""
         payload = {
@@ -88,6 +97,7 @@ class JsonrpcApi:
         response = Utils.send_http(payload)
         return response
 
+    @api_call
     def eth_getLogs(self, url, **data):
         """返回与给定过滤器对象匹配的所有日志的数组"""
         payload = {
--- conftest.py
+++ conftest.py
@@ -1,3 +1,19 @@
 """集中管理固件，pytest 会自动调用"""
+import pytest
+from loguru import logger
 
 
+# 标记测试任务的开始和结束
+@pytest.fixture(scope='session', autouse=True)
+def task_mark():
+    logger.debug("{:=^50}".format('测试任务开始'))
+    yield
+    logger.debug("{:=^50}".format('测试任务结束'))
+
+
+# 标记测试用例的开始和结束
+@pytest.fixture(autouse=True)
+def case_mark():
+    logger.debug("{:=^50}".format('用例开始'))
+    yield
+    logger.debug("{:=^50}".format('用例结束'))
